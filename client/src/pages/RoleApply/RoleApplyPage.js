08f1da68e5b0e852936c3e6ad6820174c2db790d
Mathilde Lasseigne <50111436+MathildeLasseigne@users.noreply.github.com>
2022-12-29 14:32:55 +0000

    Add Role apply page

--- client/src/pages/RoleApply/RoleApplyPage.js
+++ client/src/pages/RoleApply/RoleApplyPage.js
@@ -6,9 +6,23 @@ const RoleApplyPage = () => {
     return (
         <>
             <AppHeader className="small" />
-            <div>
-                <h1>Apply for contributor</h1>
-
+            <div className="vertical-flex align-content-center justify-content-center text-align-center margin-bottom">
+              
+              <h1>Application for contributor</h1>
+                <div>Thank you for trying to join the ranks of our contributors ! </div>
+                <div>
+                A contributor can add their own tracks to the database to allow other users to see them !
+                </div>
+                <div className="margin-top margin-bottom">
+                  To finish your application, please fill out the form below.
+                </div>
+                <form onSubmit={handleApply} className="vertical-flex align-content-center justify-content-center box big margins">
+                <label for="applicationMessage">Please explain why you want to become a contributor and how you intend to manage your tracks.</label>
+                    <textarea cols="30" rows="10" name="message" placeholder="Application message" id="applicationMessage" className="margin-top message-input"></textarea>
+                    <button type="submit" className="big button margin-top">Apply</button>
+                </form>
+                
+                       
             </div>
             <Navbar />
         </>
